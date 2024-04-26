a2236fcd3d5092b0a389099eecea4909962a8093
BrenerReis <brener.reis@fatec.sp.gov.br>
2024-04-25 21:31:18 -0300

    Adicionado tabelas
    Cursos

--- server/src/models/models.js
+++ server/src/models/models.js
@@ -33,10 +33,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  }
 });
 
 const User = mongoose.model('User', userSchema);
@@ -64,16 +60,90 @@ const partnerSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  cpf: {
+  cnpj: {
     type: String,
     required: true,
   },
   address: {
     type: String,
     required: true,
+  },
+  nivel: {
+    type: String,
+    required: true,
+  },
+  observation: {
+    type: String,
   }
 });
 
+const consultantSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdminMain: {
+    type: Boolean,
+    default: false
+  },
+  adminConsultant: {
+    type: Boolean,
+    default: false
+  },
+});
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  time: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+const courseRegistrationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course'
+  },
+  courseCompleted: {
+    type: Boolean,
+    required: true
+  },
+});
+
+const CourseRegistration = mongoose.model('CourseRegistration', courseRegistrationSchema);
+
+const Course = mongoose.model('Course', courseSchema);
+
+const Admin = mongoose.model('Admin', adminSchema);
+
 const Partner = mongoose.model('Partner', partnerSchema);
 
-module.exports = { User, userSchema, Partner, partnerSchema };
+module.exports = { User, userSchema, Partner, partnerSchema, Admin, adminSchema, Course, courseSchema, CourseRegistration, courseRegistrationSchema };
