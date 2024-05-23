bec01172be2f242e4c8e7219d8740b98af22894b
Isabel Reis <111527484+IsabelRReis@users.noreply.github.com>
2024-05-22 22:00:19 -0300

    TaskRepository

--- server/src/data/repositories/TasksRepository.js
+++ server/src/data/repositories/TasksRepository.js
@@ -1,19 +1,17 @@
 const {Task} = require('../../models/models');
 
-async function registerTask(taskData){
+
+
+async function listTasksExpertise(expertiseId) {
     try {
-        const existingTask = await Task.findOne({_id: taskData._id});
-        if(existingTask){
-            return false; // Indica que a tarefa já existe
-        }
-        const newTask = new Task(taskData);
-        await newTask.save();
-        return true; // Indica que o registro foi bem-sucedido
+        const taskRegisters = await Task.find({ expertise: expertiseId });
+        return taskRegisters;
     } catch (error) {
-        console.error('Error registering task:', error);
-        throw new Error('Failed to register task.');
+        console.error('Error listing tasks registers:', error);
+        throw new Error('Failed to list tasks registers.');
     }
 }
+
 
 
 async function listTask() {
@@ -23,19 +21,6 @@ async function listTask() {
     } catch (error) {
         console.error('Error listing task:', error);
         throw new Error('Failed to list task.');
-    }
-}
-
-async function deleteTask(taskId){
-    try {
-        const result = await Task.deleteOne({_id: taskId});
-        if (result.deletedCount === 0) {
-            throw new Error('Task não encontrada.');
-        }
-        return {message: "Task removida com sucesso."};
-    } catch (error) {
-        console.error('Erro ao deletar task:', error);
-        throw new Error('Erro interno do servidor.');     
     }
 }
 
@@ -52,15 +37,37 @@ async function updateTask(taskId, updateData){
     }
 }
 
-async function listTasksExpertise(expertiseId) {
+
+
+async function registerTask(taskData){
     try {
-        const taskRegisters = await Task.find({ expertise: expertiseId });
-        return taskRegisters;
+        const existingTask = await Task.findOne({_id: taskData._id});
+        if(existingTask){
+            return false; // Indica que a tarefa já existe
+        }
+        const newTask = new Task(taskData);
+        await newTask.save();
+        return true; // Indica que o registro foi bem-sucedido
     } catch (error) {
-        console.error('Error listing tasks registers:', error);
-        throw new Error('Failed to list tasks registers.');
+        console.error('Error registering task:', error);
+        throw new Error('Failed to register task.');
     }
 }
+
+
+async function deleteTask(taskId){
+    try {
+        const result = await Task.deleteOne({_id: taskId});
+        if (result.deletedCount === 0) {
+            throw new Error('Task não encontrada.');
+        }
+        return {message: "Task removida com sucesso."};
+    } catch (error) {
+        console.error('Erro ao deletar task:', error);
+        throw new Error('Erro interno do servidor.');     
+    }
+}
+
 
 module.exports = {
     registerTask,
