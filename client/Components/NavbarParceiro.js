c73792a2eabb4a090b1a4d9f3ecb7cb34c49e6ca
Pedro Seraggi <pedroseraggi09@gmail.com>
2024-05-23 11:44:50 -0300

    Update

--- client/Components/NavbarParceiro.js
+++ client/Components/NavbarParceiro.js
@@ -1,57 +1,68 @@
-import { View, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity, StyleSheet, Text, Animated, Dimensions, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import React, { useState, useEffect } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import Logo from '../img/LogoSemFundo.png';
 import MenuIcon from '../img/menu.png';
+import SearchIcon from '../img/search.png';
 
 const Navbar = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+  const [menuOffset] = useState(new Animated.Value(-300));
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigation = useNavigation();
 
-const [menuAberto, setMenuAberto] = useState(false);
-const navigation = useNavigation();
+  const toggleMenu = () => {
+    if (menuAberto) {
+      handleCloseMenu();
+    } else {
+      Animated.timing(menuOffset, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+      setMenuAberto(true);
+    }
+  };
 
-const toggleMenu = () => {
-  setMenuAberto(!menuAberto);
-};
+  useFocusEffect(
+    React.useCallback(() => {
+      handleCloseMenu();
+    }, [])
+  );
+
+  const handleCloseMenu = () => {
+    Animated.timing(menuOffset, {
+      toValue: -350, // Mudar para a esquerda da tela
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      setMenuAberto(false);
+    });
+  };
 
 
+  const screenHeight = Dimensions.get('window').height;
 
-useFocusEffect(
-  React.useCallback(() => {
-    handleCloseMenu();
+  return (
+    <View style={styles.header}>
+      <Image source={Logo} style={styles.logo} />
 
-  }, [])
-);
+      <TouchableOpacity style={styles.menuButton} onPress={toggleMenu}>
+        <Image source={MenuIcon} style={styles.menuIcon} />
+      </TouchableOpacity>
 
-
-const handleCloseMenu = () => {
-  setMenuAberto(false);
-};
-
-   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={toggleMenu}>
-          <Image source={MenuIcon} style={styles.menuIcon} />
-        </TouchableOpacity>
+      <Animated.View style={[styles.menu, { transform: [{ translateX: menuOffset }], height: menuAberto ? screenHeight : 50 }]}>
         <Image source={Logo} style={styles.logo} />
-      </View>
 
-      {menuAberto && (
-        <View style={styles.menu}>
-          <Text style={styles.menuText} onPress={() => navigation.navigate('TelaListaParceiro')}>Parceiros</Text>
-          <Text style={styles.menuText} onPress={() => navigation.navigate('TracksParceiro')}>Tracks</Text>
-          <Text style={styles.menuText} onPress={() => navigation.navigate('TelaParceiro')}>Perfil</Text>
-        
-        
-        </View>
-      )}
+        <Text style={styles.menuText} onPress={() => navigation.navigate('TelaListaParceiro')}>Parceiros</Text>
+        <Text style={styles.menuText} onPress={() => navigation.navigate('TracksParceiro')}>Tracks</Text>
+        <Text style={styles.menuText} onPress={() => navigation.navigate('TelaParceiro')}>Perfil</Text>
+
+      </Animated.View>
     </View>
   );
 }
-  
-
-
 
 const styles = StyleSheet.create({
   header: {
@@ -63,52 +74,91 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     zIndex: 2000,
   },
+
   logo: {
-    width: 110,
+    width: 100,
     height: 25,
     resizeMode: 'contain',
-    marginLeft: 85
+    marginLeft: 20,
+  },
+
+  menuButton: {
+    backgroundColor: '#50100c',
+    width: 70,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 200,
+    zIndex: 2001,
   },
 
   menuIcon: {
-    width: 50,
-    height: 50,
-  },
-
-  User: {
-    width: '90%',
-    height: 150,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-
-  scrollContainer: {
-    flexGrow: 1,
-    alignItems: 'center',
-    
+    width: 30,
+    height: 30,
   },
 
   menu: {
     position: 'absolute',
-    top: 70,
+    top: 0,
     left: 0,
-    width: '100%',
-    height: 200,
+    width: '70%',
     backgroundColor: '#50100c',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    zIndex: 2024,
-
-
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    zIndex: 1,
   },
 
   menuText: {
     color: 'white',
     fontFamily: 'Poppins_300Light',
-    fontSize: 16
+    fontSize: 16,
+    paddingVertical: 10,
+    marginTop: 12,
   },
 
+  searchBar: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingHorizontal: 8,
+    marginLeft: 16,
+    marginRight: 5,
+  },
+
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+
+  searchButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginLeft: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  searchIcon: {
+    width: 20,
+    height: 20,
+    tintColor: '#50100c',
+  },
+
+  filterButton: {
+    backgroundColor: '#50100c',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+
+  filterButtonText: {
+    color: 'white',
+    fontFamily: 'Poppins_300Light',
+    fontSize: 16,
+  },
 });
 
 export default Navbar;
